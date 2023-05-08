42498798d71eed488c0dc59838c93019e22a423e
ImperatorNeron <katebarspk@gmail.com>
2023-05-08 19:49:55 +0300

    test

--- ptest/settings.py
+++ ptest/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/home/ImperatorNeron/ptest/static'
+STATIC_ROOT = '/home/ImperatorNeron/ptest/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
