ab45de583f6ce2af97801ffe6060aa5d3c00bce7
ImperatorNeron <katebarspk@gmail.com>
2023-05-08 20:01:14 +0300

    test

--- ptest/settings.py
+++ ptest/settings.py
@@ -115,13 +115,12 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = '/home/ImperatorNeron/ptest/static'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'papp/static')
 ]
